32537020eb2005c05f5135d370b7a0eb60049371
wadeenghoiwa <wadee@wadeenghoiwa.com>
2021-03-22 12:05:44 +0800

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,23 @@
+document.write(`
+<div id="shadow-one">
+	<div id="shadow-two">
+	<div id="shadow-three">
+	<div id="shadow-four">
+	<div id="page" style=" background-color : FFFFFF; height: 50%;
+	  background-position: center;
+	  background-repeat: no-repeat;
+	  background-size: cover;">
 
+	<div style="padding:0 0 5px 5px"> 
+		<img src="https://www.polyu.edu.hk/assets/img/main-logo-1x.png" alt="HTML5 Icon" style="height:72px;"/>
+	</div>
+
+
+	<div id="title" style="background-color:rgb(255, 255, 31);color:black;">
+		<div class="left">TM1109 EIE IMT Project </div>
+	</div>
+
+	<div>
+		<h1>WELCOME TO INTERNET RADIO APPLICATION</h1>
+		<hr width="100%" size="4" color="436ab7" />
+`);
