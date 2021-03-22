36927e0fcec97888d6ffa81bf5eaf1a10ea73a91
wadeenghoiwa <wadee@wadeenghoiwa.com>
2021-03-22 12:08:05 +0800

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,12 @@
+document.write(`
+  	</div>
+	<script src="https://wadeenghoiwa.github.io/script.js"></script>
+	<div class="spacer">&nbsp;</div>
+
+<div id="footer">Developed by IC2140 2021 S1 </div>
+
+</div></div></div></div></div>
+</body>
+</html>
+`);
+`);
