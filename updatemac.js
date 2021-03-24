fc096443be01fa766e4ce9423a97b5bc21224843
wadeenghoiwa <wadee@wadeenghoiwa.com>
2021-03-24 09:13:47 +0800

    Update updatemac.js

--- updatemac.js
+++ updatemac.js
@@ -3,6 +3,7 @@ document.write(`
   <script src="https://wadeenghoiwa.github.io/header.js"></script> 
 
     <h1>Successful!</h1>
+    <a>MAC address saved! Power cycle or reset the board to begin using the new MAC address.</
 
   <script src="https://wadeenghoiwa.github.io/footer.js"></script> 
 `);
